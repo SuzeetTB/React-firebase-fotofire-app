8ad8b60f0756d126420dba643d4361adda4872e6
SuzeetTB <su2e3t@live.com>
2021-05-02 12:47:14 +0545

    Route FIx

--- src/App.js
+++ src/App.js
@@ -1,74 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import ImageGrid from './comps/ImageGrid';
-import Modal from './comps/Modal';
-import Title from './comps/Title';
-import UploadForm from './comps/UploadForm';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainComponent from './comps/MainComponent';
+import E404 from './comps/404'
 import './themes/switcher.scss';
 
 function App() {
-  //selected Image state
-  const [selectedImg, setSelectedImg] = useState(null);
-  //theme state hook
-  const[colorTheme, setColorTheme] = useState('theme-white');
-  //Theme selection effect
-  useEffect(() => {
-    //effect
-    //check for selected theme //local storage value
-    const currentThemeColor = localStorage.getItem('theme-color');
-    //found selected color value in state
-    if(currentThemeColor){
-      setColorTheme(currentThemeColor);
-    }
-/* 
-    return () => {
-      
-    } */
-  }, [])
-  //theme change event sets theme
-  const handleClick= (theme)=>{
-    setColorTheme(theme);
-    localStorage.setItem('theme-color',theme);
-  };
+  
 
   return (
-    <div className={`App ${colorTheme}`}>
-      <div className="head">
-      <div className="head-title"><h2>fotofire</h2></div>
-      <div className="theme-option">
-        <div id="theme-white" 
-          onClick={()=>{handleClick('theme-white')}}
-          className={`${colorTheme ==='theme-white'?'active':''}`}
-        />
-        <div id="theme-blue" 
-          onClick={()=>{handleClick('theme-blue')}}
-          className={`${colorTheme ==='theme-blue'?'active':''}`}
-        />
-        <div id="theme-orange" 
-          onClick={()=>{handleClick('theme-orange')}}
-          className={`${colorTheme ==='theme-orange'?'active':''}`}
-        />
-        <div id="theme-purple" 
-          onClick={()=>{handleClick('theme-purple')}}
-          className={`${colorTheme ==='theme-purple'?'active':''}`}
-        />
-        <div id="theme-green" 
-          onClick={()=>{handleClick('theme-green')}}
-          className={`${colorTheme ==='theme-green'?'active':''}`}
-        />
-        <div id="theme-black" 
-          onClick={()=>{handleClick('theme-black')}}
-          className={`${colorTheme ==='theme-black'?'active':''}`}
-        />
-      </div>
-      </div>
-      <div className="content-box">
-        <Title />
-        <UploadForm />
-        <ImageGrid setSelectedImg={setSelectedImg} />
-        {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
-      </div>
-
-    </div>
+    <Router basename="/react-firebase-fotofire-app">
+      <Switch>
+        <Route path='/' exact component ={MainComponent}/>
+          <Route path='*' component={E404}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/comps/404.js
@@ -0,0 +1,14 @@
+/* import { Link } from "react-router-dom" */
+
+const E404 = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry !</h2>
+            <p>The requested page may be in development</p>
+            {/* <br/>
+            <Link to="/">Home</Link> */}
+        </div>
+     );
+}
+ 
+export default E404;
