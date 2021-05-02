ff9a691d65e225b0b67024cc3b4da9b9fcc30d10
SuzeetTB <su2e3t@live.com>
2021-05-02 14:57:01 +0545

    Site finally worked

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import MainComponent from './comps/MainComponent';
 import E404 from './comps/404'
 import './themes/switcher.scss';
+import cors from 'cors';
 
 function App() {
-  
-
   return (
-    <Router basename="/">
-      <Switch>
-          <Route path='/' exact component ={MainComponent}/>
-          <Route path='*' component={E404}/>
-      </Switch>
-    </Router>
+    <MainComponent/>
+
+    
   );
 }
 
 export default App;
+
+/* <Router basename="/App">
+      <Switch>
+          <Route path='/' exact component ={MainComponent}/>
+          <Route path='*' component={E404}/>
+      </Switch>
+    </Router> */
